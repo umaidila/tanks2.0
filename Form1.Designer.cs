fc7d72097dacd56689f00d7fdb3bbbfce950095e
[umaidila] <[frenirus125@gmail.com]>
2022-05-04 02:27:32 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿using System.Windows.Forms;
+
+namespace tanks2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.infolabel1 = new System.Windows.Forms.Label();
+            this.infolabel2 = new System.Windows.Forms.Label();
+            this.infolabel3 = new System.Windows.Forms.Label();
+            this.infolabel4 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.fieldPictureBox = new System.Windows.Forms.PictureBox();
+            this.Player2Info = new System.Windows.Forms.Label();
+            this.player1Info = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fieldPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // infolabel1
+            // 
+            this.infolabel1.AutoSize = true;
+            this.infolabel1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.infolabel1.Location = new System.Drawing.Point(155, 145);
+            this.infolabel1.Name = "infolabel1";
+            this.infolabel1.Size = new System.Drawing.Size(86, 25);
+            this.infolabel1.TabIndex = 0;
+            this.infolabel1.Text = "Игрок 1";
+            // 
+            // infolabel2
+            // 
+            this.infolabel2.AutoSize = true;
+            this.infolabel2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.infolabel2.Location = new System.Drawing.Point(656, 145);
+            this.infolabel2.Name = "infolabel2";
+            this.infolabel2.Size = new System.Drawing.Size(86, 25);
+            this.infolabel2.TabIndex = 1;
+            this.infolabel2.Text = "Игрок 2";
+            // 
+            // infolabel3
+            // 
+            this.infolabel3.AutoSize = true;
+            this.infolabel3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.infolabel3.Location = new System.Drawing.Point(133, 214);
+            this.infolabel3.Name = "infolabel3";
+            this.infolabel3.Size = new System.Drawing.Size(136, 40);
+            this.infolabel3.TabIndex = 2;
+            this.infolabel3.Text = "w,a,s,d - движение\r\n\"space\" - огонь";
+            // 
+            // infolabel4
+            // 
+            this.infolabel4.AutoSize = true;
+            this.infolabel4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.infolabel4.Location = new System.Drawing.Point(627, 214);
+            this.infolabel4.Name = "infolabel4";
+            this.infolabel4.Size = new System.Drawing.Size(147, 40);
+            this.infolabel4.TabIndex = 3;
+            this.infolabel4.Text = "стрелки - движение\r\n\"enter\" - огонь\r\n";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(406, 318);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(93, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Начать игру";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // fieldPictureBox
+            // 
+            this.fieldPictureBox.Location = new System.Drawing.Point(8, 41);
+            this.fieldPictureBox.Name = "fieldPictureBox";
+            this.fieldPictureBox.Size = new System.Drawing.Size(1024, 576);
+            this.fieldPictureBox.TabIndex = 5;
+            this.fieldPictureBox.TabStop = false;
+            this.fieldPictureBox.Visible = false;
+            // 
+            // Player2Info
+            // 
+            this.Player2Info.AutoSize = true;
+            this.Player2Info.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Player2Info.ForeColor = System.Drawing.Color.Blue;
+            this.Player2Info.Location = new System.Drawing.Point(844, 9);
+            this.Player2Info.Name = "Player2Info";
+            this.Player2Info.Size = new System.Drawing.Size(96, 25);
+            this.Player2Info.TabIndex = 6;
+            this.Player2Info.Text = "Игрок 2: ";
+            this.Player2Info.Visible = false;
+            // 
+            // player1Info
+            // 
+            this.player1Info.AutoSize = true;
+            this.player1Info.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player1Info.ForeColor = System.Drawing.Color.Red;
+            this.player1Info.Location = new System.Drawing.Point(51, 9);
+            this.player1Info.Name = "player1Info";
+            this.player1Info.Size = new System.Drawing.Size(96, 25);
+            this.player1Info.TabIndex = 6;
+            this.player1Info.Text = "Игрок 1: ";
+            this.player1Info.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 629);
+            this.Controls.Add(this.player1Info);
+            this.Controls.Add(this.Player2Info);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.infolabel4);
+            this.Controls.Add(this.infolabel3);
+            this.Controls.Add(this.infolabel2);
+            this.Controls.Add(this.infolabel1);
+            this.Controls.Add(this.fieldPictureBox);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.fieldPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infolabel1;
+        private System.Windows.Forms.Label infolabel2;
+        private System.Windows.Forms.Label infolabel3;
+        private System.Windows.Forms.Label infolabel4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox fieldPictureBox;
+        private System.Windows.Forms.Label Player2Info;
+        private System.Windows.Forms.Label player1Info;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,422 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace tanks2._0
+{
+    public partial class Form1 : Form
+    {
+        public struct obstacle {
+            public int x;
+            public int y;
+            public obstacle(int X, int Y)
+            {
+                x = X;
+                y = Y;
+            }
+        };
+
+        public struct tank
+        {
+            public int x;
+            public int y;
+            public int hp;
+            public int direction; // 0 - up, 1 - right, 2 - down, 3 - left
+            public bool move;
+
+        }
+
+        public struct bullet
+        {
+            public int x;
+            public int y;
+            public int direction;
+            public bool active;
+        }
+
+        List<obstacle> listObstacles;
+
+        private Bitmap picBitmap;
+        private Bitmap staticBitmap;
+        private Pen p;
+        private SolidBrush b;
+        private SolidBrush b1;
+        private SolidBrush b2;
+        private Graphics g;
+        tank player1;
+        tank player2;
+        bullet bullet1;
+        bullet bullet2;
+        int setHp = 3;
+        List<tank> players;
+        int speed = 4;
+        int tickIndex = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            picBitmap = new Bitmap(fieldPictureBox.Width, fieldPictureBox.Height);
+            listObstacles = new List<obstacle>(48);
+            player1 = new tank();
+            player2 = new tank();
+            bullet1 = new bullet();
+            bullet2 = new bullet();
+            bullet1.active = false;
+            bullet2.active = false;
+            player1.hp = setHp;
+            player2.hp = setHp;
+            player1.move = false;
+            player2.move = false;
+            player1.direction = 1;
+            player2.direction = 3;
+            player1.x = 64;
+            player1.y = 64;
+            player2.x = 896;
+            player2.y = 288;
+            players = new List<tank> { player1, player2 };
+            {                
+                listObstacles.Add(new obstacle(9, 0));
+                listObstacles.Add(new obstacle(9, 1));
+                listObstacles.Add(new obstacle(9, 2));
+                listObstacles.Add(new obstacle(9, 3));
+                listObstacles.Add(new obstacle(9, 4));
+                listObstacles.Add(new obstacle(8, 4));
+                listObstacles.Add(new obstacle(7, 4));
+                listObstacles.Add(new obstacle(6, 4));
+                listObstacles.Add(new obstacle(13, 2));
+                listObstacles.Add(new obstacle(14, 2));
+                listObstacles.Add(new obstacle(15, 2));
+                listObstacles.Add(new obstacle(16, 2));
+                listObstacles.Add(new obstacle(17, 2));
+                listObstacles.Add(new obstacle(18, 2));
+                listObstacles.Add(new obstacle(26, 2));
+                listObstacles.Add(new obstacle(26, 3));
+                listObstacles.Add(new obstacle(3, 10));
+                listObstacles.Add(new obstacle(3, 11));
+                listObstacles.Add(new obstacle(4, 10));
+                listObstacles.Add(new obstacle(4, 11));
+                listObstacles.Add(new obstacle(8, 11));
+                listObstacles.Add(new obstacle(8, 12));
+                listObstacles.Add(new obstacle(8, 13));
+                listObstacles.Add(new obstacle(9, 13));
+                listObstacles.Add(new obstacle(12, 10));
+                listObstacles.Add(new obstacle(13, 10));
+                listObstacles.Add(new obstacle(13, 9));
+                listObstacles.Add(new obstacle(13, 8));
+                listObstacles.Add(new obstacle(13, 7));
+                listObstacles.Add(new obstacle(14, 7));
+                listObstacles.Add(new obstacle(14, 6));
+                listObstacles.Add(new obstacle(17, 12));
+                listObstacles.Add(new obstacle(17, 13));
+                listObstacles.Add(new obstacle(20, 6));
+                listObstacles.Add(new obstacle(21, 6));
+                listObstacles.Add(new obstacle(20, 7));
+                listObstacles.Add(new obstacle(20, 8));
+                listObstacles.Add(new obstacle(20, 9));
+                listObstacles.Add(new obstacle(21, 9));
+                listObstacles.Add(new obstacle(23, 13));
+                listObstacles.Add(new obstacle(26, 13));
+                listObstacles.Add(new obstacle(26, 12));
+                listObstacles.Add(new obstacle(23, 14));
+                listObstacles.Add(new obstacle(24, 14));
+                listObstacles.Add(new obstacle(25, 14));
+                listObstacles.Add(new obstacle(26, 14));
+                listObstacles.Add(new obstacle(23, 15));
+                listObstacles.Add(new obstacle(24, 15));
+                listObstacles.Add(new obstacle(25, 15));
+                listObstacles.Add(new obstacle(26, 15));
+            }
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {   
+            infolabel1.Visible = false;
+            infolabel2.Visible = false;
+            infolabel3.Visible = false;
+            infolabel4.Visible = false;
+            startButton.Visible = false;
+            player1Info.Visible = true;
+            Player2Info.Visible = true;
+            fieldPictureBox.Visible = true;
+
+            player1Info.Text = "Игрок 1:  " + setHp;
+            Player2Info.Text = "Игрок 2:  " + setHp;
+
+            p = new Pen(Color.Black, 2);
+            b = new SolidBrush(Color.FromArgb(166, 242, 150));
+            g = Graphics.FromImage(picBitmap);
+
+            g.FillRectangle(b, 0, 0, fieldPictureBox.Width, fieldPictureBox.Height);
+
+            b.Color = Color.FromArgb(92, 33, 4);
+
+            for (int i = 0; i < 32; i++) // верхняя и нижняя граница
+            {
+                g.FillRectangle(b, i * 32, 0, 32, 32);
+                g.DrawRectangle(p, i * 32, 0, 32, 32);
+                g.FillRectangle(b, i * 32, 544, 32, 32);
+                g.DrawRectangle(p, i * 32, 544, 32, 32);           
+            }
+            for (int i = 1; i < 17; i++) // левая и правая граница
+            {
+                g.FillRectangle(b, 0, i * 32, 32, 32);
+                g.DrawRectangle(p, 0, i * 32, 32, 32);
+                g.FillRectangle(b, 992, i * 32, 32, 32);
+                g.DrawRectangle(p, 992, i * 32, 32, 32);               
+            }
+
+            foreach (var obst in listObstacles)
+            {
+                g.FillRectangle(b, 32 + obst.x * 32, 32 + obst.y * 32, 32, 32);
+                g.DrawRectangle(p, 32 + obst.x * 32, 32 + obst.y * 32, 32, 32);
+            }
+
+            staticBitmap = (Bitmap)picBitmap.Clone();
+
+            fieldPictureBox.Image = picBitmap;
+
+            timer1.Enabled = true;
+            render();
+
+            
+        }
+
+        private void render()
+        {
+            //picBitmap.Dispose();
+            picBitmap = (Bitmap)staticBitmap.Clone();
+            g = Graphics.FromImage(picBitmap);
+            p = new Pen(Color.Black, 2);
+            b1 = new SolidBrush(Color.FromArgb(255, 0,0));
+            b2 = new SolidBrush(Color.FromArgb(0,0,255));
+
+            g.FillRectangle(b1, player1.x, player1.y, 32, 32);
+            g.DrawRectangle(p, player1.x, player1.y, 32, 32);
+            g.FillRectangle(b2, player2.x, player2.y, 32, 32);
+            g.DrawRectangle(p, player2.x, player2.y, 32, 32);
+
+            switch (player1.direction) 
+            {
+                case 0:
+                    g.DrawLine(p, player1.x + 16, player1.y, player1.x + 16, player1.y - 16);
+                    break;
+                case 1:
+                    g.DrawLine(p, player1.x + 32, player1.y + 16, player1.x + 48, player1.y + 16);
+                    break;
+                case 2:
+                    g.DrawLine(p, player1.x + 16, player1.y + 32, player1.x + 16, player1.y + 48);
+                    break;
+                case 3:
+                    g.DrawLine(p, player1.x, player1.y + 16, player1.x - 16, player1.y + 16);
+                    break;
+            }
+
+            switch (player2.direction)
+            {
+                case 0:
+                    g.DrawLine(p, player2.x + 16, player2.y, player2.x + 16, player2.y - 16);
+                    break;
+                case 1:
+                    g.DrawLine(p, player2.x + 32, player2.y + 16, player2.x + 48, player2.y + 16);
+                    break;
+                case 2:
+                    g.DrawLine(p, player2.x + 16, player2.y + 32, player2.x + 16, player2.y + 48);
+                    break;
+                case 3:
+                    g.DrawLine(p, player2.x, player2.y + 16, player2.x - 16, player2.y + 16);
+                    break;
+            }
+
+            fieldPictureBox.Image = picBitmap;
+            tickIndex++;
+            if (tickIndex > 50)
+            {
+                tickIndex = 0;
+                GC.Collect();
+            }
+        }
+
+        int getCoords(int t)
+        {
+            return 32 + t * 32;
+        }
+
+        private bool checkBorders(int x,int y, int direction)
+        {
+            switch (direction)
+            {
+                case 0:
+                    if (y <= 32)
+                        return false;
+                    foreach (var obst in listObstacles)
+                        if (y <= getCoords(obst.y)+32 && ((x + 32 >= getCoords(obst.x) && x+ 32 <= getCoords(obst.x) + 32)||(x >= getCoords(obst.x) && x <= getCoords(obst.x) + 32)))
+                            return false;
+                    break;
+                case 1:
+                    if (x + 32 >= 992)
+                        return false;
+                    foreach (var obst in listObstacles)
+                        if (x + 32 >= getCoords(obst.x) && ((y + 32 >= getCoords(obst.y) && y + 32 <= getCoords(obst.y) + 32) || (y >= getCoords(obst.y) && y <= getCoords(obst.y) + 32)))
+                            return false;
+                    break;
+                case 2:
+                    if (y + 32 >= 544)
+                        return false;
+                    foreach (var obst in listObstacles)
+                        if (y + 32 >=  getCoords(y) && ((x + 32 >= getCoords(obst.x) && x + 32 <= getCoords(obst.x) + 32) || (x >= getCoords(obst.x) && x <= getCoords(obst.x) + 32)))
+                            return false;
+                    break;
+                case 3:
+                    if (x <= 32)
+                        return false;
+                    foreach (var obst in listObstacles)
+                        if (x <= getCoords(obst.x)+ 32 && ((x + 32 >= getCoords(obst.x) && x + 32 <= getCoords(obst.x) + 32) || (x >= getCoords(obst.x) && x <= getCoords(obst.x) + 32)))
+                            return false;
+                    break;
+            }
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (player1.move)
+            {
+                switch (player1.direction)
+                {
+                    case 0:
+                        if (checkBorders(player1.x,player1.y,player1.direction))
+                            player1.y -= speed;
+                        break;
+                    case 1:
+                        if (checkBorders(player1.x, player1.y, player1.direction))                          
+                            player1.x += speed;
+                        break;
+                    case 2:
+                        if (checkBorders(player1.x, player1.y, player1.direction))                            
+                            player1.y += speed;
+                        break;
+                    case 3:
+                        if (checkBorders(player1.x, player1.y, player1.direction))                          
+                            player1.x -= speed;
+                        break;
+                }
+                
+            }
+            if (player2.move)
+            {
+                switch (player2.direction)
+                {
+                    case 0:
+                        if (checkBorders(player2.x, player2.y, player2.direction))                           
+                            player2.y -= speed;
+                        break;
+                    case 1:
+                        if (checkBorders(player2.x, player2.y, player2.direction))                            
+                            player2.x += speed;
+                        break;
+                    case 2:
+                        if (checkBorders(player2.x, player2.y, player2.direction))
+                            player2.y += speed;
+                        break;
+                    case 3:
+                        if (checkBorders(player2.x, player2.y, player2.direction))
+                            player2.x -= speed;
+                        break;
+                }
+            }
+            render();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {           
+            if (e.KeyCode == Keys.W && player1.move == false)
+            {
+                player1.direction = 0;
+                player1.move = true;
+            }
+            if (e.KeyCode == Keys.D && player1.move == false)
+            {
+                player1.direction = 1;
+                player1.move = true;
+            }
+            if (e.KeyCode == Keys.S && player1.move == false)
+            {
+                player1.direction = 2;
+                player1.move = true;
+            }
+            if (e.KeyCode == Keys.A && player1.move == false)
+            {
+                player1.direction = 3;
+                player1.move = true;
+            }
+
+            if (e.KeyCode == Keys.Up && player2.move == false)
+            {
+                player2.direction = 0;
+                player2.move = true;
+            }
+            if (e.KeyCode == Keys.Right && player2.move == false)
+            {
+                player2.direction = 1;
+                player2.move = true;
+            }
+            if (e.KeyCode == Keys.Down && player2.move == false)
+            {
+                player2.direction = 2;
+                player2.move = true;
+            }
+            if (e.KeyCode == Keys.Left && player2.move == false)
+            {
+                player2.direction = 3;
+                player2.move = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W && player1.direction == 0)
+            {
+                player1.move = false;
+            }
+            if (e.KeyCode == Keys.D && player1.direction == 1)
+            {
+                player1.move = false;
+            }
+            if (e.KeyCode == Keys.S && player1.direction == 2)
+            {
+                player1.move = false;
+            }
+            if (e.KeyCode == Keys.A && player1.direction == 3)
+            {
+                player1.move = false;
+            }
+
+            if (e.KeyCode == Keys.Up && player2.direction == 0)
+            {
+                player2.move = false;
+            }
+            if (e.KeyCode == Keys.Right && player2.direction == 1)
+            {
+                player2.move = false;
+            }
+            if (e.KeyCode == Keys.Down && player2.direction == 2)
+            {
+                player2.move = false;
+            }
+            if (e.KeyCode == Keys.Left && player2.direction == 3)
+            {
+                player2.move = false;
+            }
+        }
+    }
+}
